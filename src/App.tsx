14af126412553ae36d90f143f3bbd50f783accf6
Justin Gebert <justin.gebert.berlin@gmail.com>
2024-11-10 00:19:10 +0100

    set up clean project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from './components/mode-toggle'
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle/>
+    </ThemeProvider>
+  )
+}
+
+export default App
